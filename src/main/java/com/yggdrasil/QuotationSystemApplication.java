cd969bb77cd296ac435c1d8d4f3d91a9785585b7
Yggdrasilqh <yggdrasilqh@gmail.com>
2017-03-31 21:28:53 +0800

    init Project

---
+++ src/main/java/com/yggdrasil/QuotationSystemApplication.java
@@ -0,0 +1,12 @@
+package com.yggdrasil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotationSystemApplication.class, args);
+	}
+}
